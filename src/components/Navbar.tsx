7141369c99162dd0fc26d65da72d192a422fe105
Lavinia Thosatriavi <lthosatriavi@gmail.com>
2025-01-09 11:45:14 +0900

    initial navbar design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,11 @@
-import React from "react";
+import { useState } from "react";
 import styled from "styled-components";
+import logo from "../assets/Logo.png";
+import { FaGlobe } from "react-icons/fa";
+
+interface LanguageDropdownProps {
+  show: boolean;
+}
 
 const Container = styled.div`
   position: fixed;
@@ -8,11 +14,90 @@ const Container = styled.div`
   width: 100%;
   height: 72px;
   background-color: #016532;
-  padding: 0 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  z-index: 1000;
+`;
+
+const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Logo = styled.img`
+  width: 60px;
+  height: 66px;
+  margin-right: ;
+`;
+
+const Title = styled.h1`
+  color: white;
+  font-size: 28px;
+  font-family: "Roboto", sans-serif;
+  font-weight: 500; /* Medium weight */
+  margin: 0;
+`;
+
+const RightContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const GlobeIcon = styled(FaGlobe)`
+  color: white;
+  font-size: 32px;
+  cursor: pointer;
+
+  &:hover {
+    color: #fc5600;
+  }
+`;
+
+const LanguageDropdown = styled.div<LanguageDropdownProps>`
+  position: absolute;
+  background-color: white;
+  border: 1px solid black;
+  overflow: hidden;
+  display: ${(props) => (props.show ? "block" : "none")};
+`;
+
+const DropdownOption = styled.div`
+  font-size: 16px;
+  color: black;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #e0e0e0;
+  }
 `;
 
 const Navbar = () => {
-  return <Container></Container>;
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
+  const closeDropdown = () => {
+    setDropdownOpen(false);
+  };
+
+  return (
+    <Container>
+      <LogoContainer>
+        <Logo src={logo} alt="EduHK Logo" />
+        <Title>Student Collaboration Learning System</Title>
+      </LogoContainer>
+      <RightContainer>
+        <GlobeIcon onClick={toggleDropdown} />
+        <LanguageDropdown show={dropdownOpen}>
+          <DropdownOption onClick={closeDropdown}>简体中文</DropdownOption>
+          <DropdownOption onClick={closeDropdown}>English</DropdownOption>
+        </LanguageDropdown>
+      </RightContainer>
+    </Container>
+  );
 };
 
 export default Navbar;
