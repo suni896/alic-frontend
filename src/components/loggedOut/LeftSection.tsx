89a84e94e56beec298a0140a48b833dd2ea146a4
Lavinia Thosatriavi <lthosatriavi@gmail.com>
2025-01-13 15:39:00 +0900

    left sectionreusable component, collaborative learning

---
+++ src/components/loggedOut/LeftSection.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import styled from "styled-components";
+
+interface LeftSectionProps {
+  currentImage: string;
+  currentIndex: number; // Include currentIndex
+  images: string[]; // Include images array
+}
+
+const LeftSectionContainer = styled.div<{ currentImage: string }>`
+  flex: 1.15;
+  height: 100%;
+  position: relative;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-image: url(${(props) => props.currentImage});
+    background-size: cover;
+    background-position: center;
+    opacity: 0.75;
+    z-index: 1;
+  }
+`;
+
+const LeftTitle = styled.h1`
+  position: absolute;
+  top: 35px;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+  text-align: center;
+  color: black;
+  font-size: 1.5rem;
+  font-family: "Roboto Condensed", sans-serif;
+  font-weight: 500;
+  font-style: italic;
+  z-index: 2;
+`;
+
+const DotsContainer = styled.div`
+  position: absolute;
+  bottom: 20px;
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
+  z-index: 2;
+`;
+
+const Dot = styled.div<{ active: boolean }>`
+  width: 15px;
+  height: 15px;
+  border-radius: 50%;
+  background-color: ${(props) => (props.active ? "#016532" : "white")};
+  margin: 0 5px;
+`;
+
+const LeftSection: React.FC<LeftSectionProps> = ({
+  currentImage,
+  currentIndex,
+  images,
+}) => {
+  const title = "Join our community today for collaborative learning!";
+
+  return (
+    <LeftSectionContainer currentImage={currentImage}>
+      <LeftTitle>{title}</LeftTitle>
+      <DotsContainer>
+        {images.map((_, index: number) => (
+          <Dot key={index} active={index === currentIndex} />
+        ))}
+      </DotsContainer>
+    </LeftSectionContainer>
+  );
+};
+
+export default LeftSection;
--- src/components/loggedOut/Signin.tsx
+++ src/components/loggedOut/Signin.tsx
@@ -1,34 +1,57 @@
+import { useState, useEffect } from "react";
 import styled from "styled-components";
+import image1 from "../../assets/collaborativeLearning-1.png";
+import image2 from "../../assets/collaborativeLearning-2.png";
+import image3 from "../../assets/collaborativeLearning-3.png";
+import LeftSection from "./LeftSection"; // Adjust the path as necessary
 
 const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100vh;
-  background-color: #f4f4f4;
+  background: white;
+  margin-top: 30px;
 `;
 
 const Wrapper = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
   width: 90%;
+  height: 70%;
   max-width: 1300px;
   background: white;
   overflow: hidden;
+  border: 1px solid #016532;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1); /* Drop shadow */
 `;
 
-const LeftSection = styled.div`
+const images = [image1, image2, image3];
+
+const RightSection = styled.div`
   flex: 1;
-  background-image: url("https://via.placeholder.com/600x400");
-  background-size: cover;
-  background-position: center;
-  display: flex;
 `;
 
 const Signin = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 2000);
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <Container>
       <Wrapper>
-        <LeftSection></LeftSection>
+        <LeftSection
+          currentImage={images[currentIndex]}
+          currentIndex={currentIndex}
+          images={images}
+        />
+        <RightSection></RightSection>
       </Wrapper>
     </Container>
   );
